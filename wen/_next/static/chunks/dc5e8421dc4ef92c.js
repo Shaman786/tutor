bab7e519a49c1c7b911cd811849e82a553c1d0da
Fayaj Hossain <fayazhossain88@gmail.com>
2025-10-04 21:18:30 +0530

    Add remaining files and updates

---
+++ wen/_next/static/chunks/dc5e8421dc4ef92c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/6b297c6bc770653d.js",
+  "static/chunks/19f67396548ab510.js",
+  "static/chunks/turbopack-f4dc3518dd483d52.js"
+])
---
+++ wen/_next/static/chunks/e15abd352e73651c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/5a6c3db863a354ce.js",
+  "static/chunks/19f67396548ab510.js",
+  "static/chunks/turbopack-1633202c9f5e592e.js"
+])
